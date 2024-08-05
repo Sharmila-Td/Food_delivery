12f073ecb277b4956a209051b8811e245e0a179d
sharmila-developer <sharmi.webdev@gmail.com>
2024-08-04 21:28:31 -0700

    footer comp

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import Navbar from './Components/Navbar/Navbar';
 import Home from './Components/Home/Home';
 import Cart from './Components/Cart/Cart';
 import PlaceOrder from './Components/PlaceOrder/PlaceOrder';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
+    <>
     <div className="App">
        <Navbar/>
        <Routes>
@@ -14,6 +16,8 @@ function App() {
           <Route path='/order' element={<PlaceOrder/>}/>
        </Routes>
     </div>
+      <Footer/>
+    </>
   );
 }
 
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,9 +1,45 @@
 import React from 'react';
 import './Footer.css';
+import { BiEnvelope, BiPhone, BiAlarm, BiLocationPlus } from 'react-icons/bi';
+import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa6';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer' id='footer'>
+        <div className='footer_content_one'>
+            <div className='footer_left'>
+                  <h3> Food Quality Description </h3>
+                  <p>hi hello its food quality description section it would most important section that woukd </p>
+                  <p> Timing  24 x 7  <BiAlarm/> </p>
+                  <ul className='footer_social-icon'>
+                    <li> <FaFacebook /> </li>
+                    <li> <FaTwitter/> </li>
+                    <li> <FaInstagram/> </li>
+                  </ul>
+            </div>
+            <div className='footer_center'>
+                  <h3>Resources</h3>
+                  <ul>
+                    <li>Shipping Policy</li>
+                    <li>Privacy Policy</li>
+                    <li>Blogs</li>
+                    <li>Sitemap</li>
+                  </ul>
+            </div>
+            <div className='footer_right'>
+                <h3>Contact us</h3>
+                <ul>
+                  <li> <BiEnvelope/>  anufoods@gmail.com </li>
+                  <li> <BiPhone/>  +91 785 653 514 </li>
+                  <li>  < BiLocationPlus/> No 15, Anna Nagar <br/>
+                  Chennai - 987456 </li>
+                </ul>
+            </div>
+        </div>
+        <hr/>
+       <p className='footer_copyright'> Copyright 2024 &copy; Anu.com - All Rights Reserved</p>
+        
+    </div>
   )
 }
 
