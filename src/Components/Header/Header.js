ce5dde2a20f629bff8b01d81ee6d63f2737829fc
sharmila-developer <sharmi.webdev@gmail.com>
2024-08-08 20:57:21 -0700

    home page modification

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './Header.css';
+import { BsArrowDown } from 'react-icons/bs';
+
 
 
 const Header = () => {
   return (
     <div className='header'>
         <div className='header_content'> 
-            <h3>Order your Favourite food here !</h3>
-            <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise.</p>
-            <a href='#' className='btn'>View menu</a>
+            <h3>Order <br/> your <br/>  Favourite <br/> food <br/> here</h3>
+            <a href='#' className='btn'>View menu <BsArrowDown/> </a> 
         </div>
     </div>
   )
